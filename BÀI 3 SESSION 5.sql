5965e9906edc46f10113abe392add798d84d2357
lkhavan2203-source <lkhavan2203@gmail.com>
2025-11-14 09:43:04 +0700

    Add files via upload

---
+++ BÀI 3 SESSION 5.sql
@@ -0,0 +1,40 @@
+CREATE TABLE DonHang (
+
+    MaDonHang INT,
+
+    NgayDatHang DATE,
+
+    TongGiaTri INT,
+
+    ThanhPhoGiaoHang VARCHAR(100)
+
+);
+
+ 
+
+INSERT INTO DonHang (MaDonHang, NgayDatHang, TongGiaTri, ThanhPhoGiaoHang) VALUES
+
+(101, '2023-10-15', 1500000, 'Hà Nội'),
+
+(102, '2023-10-18', 800000, 'Đà Nẵng'),
+
+(103, '2023-10-20', 2200000, 'TP. Hồ Chí Minh'),
+
+(104, '2023-10-22', 750000, 'Hà Nội'),
+
+(105, '2023-10-25', 3100000, 'TP. Hồ Chí Minh');
+-- Liệt kê đơn hàng có tổng giá trị > 1,000,000
+SELECT *
+FROM DonHang
+WHERE TongGiaTri > 1000000;
+-- Liệt kê đơn hàng được giao đến 'TP. Hồ Chí Minh'
+SELECT *
+FROM DonHang
+WHERE ThanhPhoGiaoHang = 'TP. Hồ Chí Minh';
+-- Liệt kê và sắp xếp đơn hàng theo TongGiaTri giảm dần
+SELECT *
+FROM DonHang
+ORDER BY TongGiaTri DESC;
+-- Liệt kê danh sách các thành phố (duy nhất) đã có đơn hàng
+SELECT DISTINCT ThanhPhoGiaoHang
+FROM DonHang;
